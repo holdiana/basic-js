49c94f38f83c86004713ec56cb71c96d93ff1748
Diana Holoborodko <diana.holoborodko@gmail.com>
2021-03-28 20:02:29 +0300

    feat: resolved count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -3,11 +3,11 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function countCats(backyard) {
 	if (!Array.isArray(backyard)) return 0;
 	let count = 0;
-	console.log();
+
 	for (let i = 0; i < backyard.length; i++) {
 		for (let j = 0; j < backyard[i].length; j++) {
 			if (backyard[i][j] === "^^") {
-				count++;
+				count += 1;
 			}
 		}
 	}
