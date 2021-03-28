c5133afc601f06b068042837040bf8d0b5322ef0
Diana Holoborodko <diana.holoborodko@gmail.com>
2021-03-28 21:24:22 +0300

    feat: solved recursive depth

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+	let activity = parseFloat(sampleActivity);
+	if (typeof sampleActivity !== 'string' || isNaN(activity) || activity <= 0 || activity > MODERN_ACTIVITY) {
+		return false;
+	}
+	let ln2 = 0.693;
+	let result = Math.ceil(Math.log(MODERN_ACTIVITY / activity) / (ln2 / HALF_LIFE_PERIOD));
+	return result;
+
 };
